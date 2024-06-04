238b9b0e370d5ac4573843450fe2563c42d5eff0
yuetang1 <yue.tang.2@stonybrook.edu>
2024-06-03 22:55:34 -0400

    project set up

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center">
+      <header className="bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold">
+          Welcome to Tailwind CSS with React!
+        </h1>
+      </header>
+      <main className="p-4">
+        <p className="text-lg">
+          This is a simple example of using Tailwind CSS in a React application.
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
